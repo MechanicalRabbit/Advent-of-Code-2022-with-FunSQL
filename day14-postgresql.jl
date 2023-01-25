26f748fed5ea974b1f1629191a1eea3e37653eba
Kyrylo Simonov <xi@resolvent.net>
2023-01-25 23:46:12 +0000

    Day 14 with PostgreSQL

---
+++ day14-postgresql.jl
@@ -0,0 +1,173 @@
+using FunSQL
+using LibPQ
+using DBInterface
+
+# Make LibPQ compatible with DBInterface.
+DBInterface.connect(::Type{LibPQ.Connection}, args...; kws...) =
+    LibPQ.Connection(args...; kws...)
+
+DBInterface.prepare(conn::LibPQ.Connection, args...; kws...) =
+    LibPQ.prepare(conn, args...; kws...)
+
+DBInterface.execute(conn::Union{LibPQ.Connection, LibPQ.Statement}, args...; kws...) =
+    LibPQ.execute(conn, args...; kws...)
+
+@funsql begin
+
+with_ordinality(q) =
+    `? WITH ORDINALITY`(q)
+
+array_get(a, i) =
+    `?[?]`(a, i)
+
+as_integer(str) =
+    `(?::integer)`(str)
+
+parse_segments() =
+    begin
+        from(
+            with_ordinality(string_to_table(:input, "\n")),
+            columns = [path, path_index])
+        cross_join(
+            from(
+                with_ordinality(regexp_matches(path, "(\\d+),(\\d+)", "g")),
+                columns = [captures, point_index]))
+        define(
+            x => as_integer(array_get(captures, 1)),
+            y => as_integer(array_get(captures, 2)))
+        partition(path_index, order_by = [point_index])
+        filter(point_index > 1)
+        define(
+            prev_x => lag[x],
+            prev_y => lag[y])
+        define(
+            x1 => least(x, prev_x),
+            x2 => greatest(x, prev_x),
+            y1 => least(y, prev_y),
+            y2 => greatest(y, prev_y))
+    end
+
+on_segment(X, Y) =
+    exists(
+        begin
+            from(segments)
+            filter(between(:X, x1, x2) && between(:Y, y1, y2))
+            bind(:X => X, :Y => Y)
+        end)
+
+max_y() =
+    begin
+        from(segments)
+        group()
+        define(max_y => max[y2] + 1)
+    end
+
+reachable_step() =
+    begin
+        cross_join(from(max_y))
+        filter(y < max_y)
+        cross_join(from((; dx = [-1, 0, 1])))
+        group(
+            x => x + dx,
+            y => y + 1)
+        filter(!on_segment(x, y))
+    end
+
+reachable() =
+    begin
+        define(
+            x => 500,
+            y => 0)
+        iterate(reachable_step())
+    end
+
+is_reachable(X, Y) =
+    exists(
+        begin
+            from(reachable)
+            filter(x == :X && y == :Y)
+            bind(:X => X, :Y => Y)
+        end)
+
+fallthrough_step() =
+    begin
+        cross_join(from((; dx = [-1, 0, 1])))
+        group(
+            x => x + dx,
+            y => y - 1)
+        filter(is_reachable(x, y))
+    end
+
+fallthrough() =
+    begin
+        from(reachable)
+        cross_join(from(max_y))
+        filter(y == max_y)
+        iterate(fallthrough_step())
+    end
+
+is_fallthrough(X, Y) =
+    exists(
+        begin
+            from(fallthrough)
+            filter(x == :X && y == :Y)
+            bind(:X => X, :Y => Y)
+        end)
+
+resting_step() =
+    begin
+        define(
+            down_rest => !is_fallthrough(x, y + 1),
+            down_left_rest => !is_fallthrough(x - 1, y + 1))
+        cross_join(from((; dx = [-1, 0, 1])))
+        define(
+            x => x + dx,
+            y => y + 1)
+        filter(
+            dx == 0 ||
+            dx == -1 && down_rest ||
+            dx == 1 && down_rest && down_left_rest)
+        group(x, y)
+        filter(is_reachable(x, y))
+    end
+
+solve_part1() =
+    begin
+        define(
+            x => 500,
+            y => 0)
+        iterate(resting_step())
+        filter(!is_fallthrough(x, y))
+        group()
+        define(part1 => count[])
+    end
+
+solve_part2() =
+    begin
+        from(reachable)
+        group()
+        define(part2 => count[])
+    end
+
+solve_all() =
+    let segments = parse_segments(),
+        max_y = max_y(),
+        reachable = reachable(),
+        fallthrough = fallthrough()
+        solve_part1().cross_join(solve_part2())
+    end
+
+const q = solve_all()
+
+end # @funsql
+
+if isempty(ARGS)
+    println(FunSQL.render(q, dialect = :postgresql))
+else
+    const db = DBInterface.connect(FunSQL.DB{LibPQ.Connection}, "")
+    for file in ARGS
+        input = read(file, String)
+        output = first(DBInterface.execute(db, q, input = input))
+        println("[$file] part1: $(output.part1), part2: $(output.part2)")
+    end
+end
---
+++ day14-postgresql.sql
@@ -0,0 +1,187 @@
+WITH RECURSIVE "segments_1" ("x1", "x2", "y1", "y2") AS (
+  SELECT
+    least("__4"."x", "__4"."lag") AS "x1",
+    greatest("__4"."x", "__4"."lag") AS "x2",
+    least("__4"."y", "__4"."lag_2") AS "y1",
+    greatest("__4"."y", "__4"."lag_2") AS "y2"
+  FROM (
+    SELECT
+      "__3"."x",
+      "__3"."y",
+      (lag("__3"."x") OVER (PARTITION BY "__3"."path_index" ORDER BY "__3"."point_index")) AS "lag",
+      (lag("__3"."y") OVER (PARTITION BY "__3"."path_index" ORDER BY "__3"."point_index")) AS "lag_2",
+      "__3"."point_index"
+    FROM (
+      SELECT
+        ("__2"."captures"[1]::integer) AS "x",
+        ("__2"."captures"[2]::integer) AS "y",
+        "__2"."point_index",
+        "__1"."path_index"
+      FROM string_to_table($1, '
+') WITH ORDINALITY AS "__1" ("path", "path_index")
+      CROSS JOIN regexp_matches("__1"."path", '(\d+),(\d+)', 'g') WITH ORDINALITY AS "__2" ("captures", "point_index")
+    ) AS "__3"
+  ) AS "__4"
+  WHERE ("__4"."point_index" > 1)
+),
+"max_y_1" ("max_y") AS (
+  SELECT (max("segments_2"."y2") + 1) AS "max_y"
+  FROM "segments_1" AS "segments_2"
+),
+"__5" ("x", "y") AS (
+  SELECT
+    500 AS "x",
+    0 AS "y"
+  UNION ALL
+  SELECT
+    "__8"."x",
+    "__8"."y"
+  FROM (
+    SELECT DISTINCT
+      ("__7"."x" + "values_2"."dx") AS "x",
+      ("__7"."y" + 1) AS "y"
+    FROM (
+      SELECT
+        "__6"."x",
+        "__6"."y"
+      FROM "__5" AS "__6"
+      CROSS JOIN "max_y_1" AS "max_y_2"
+      WHERE ("__6"."y" < "max_y_2"."max_y")
+    ) AS "__7"
+    CROSS JOIN (
+      SELECT "values_1"."dx"
+      FROM (
+        VALUES
+          (-1),
+          (0),
+          (1)
+      ) AS "values_1" ("dx")
+    ) AS "values_2"
+  ) AS "__8"
+  WHERE (NOT (EXISTS (
+    SELECT NULL
+    FROM "segments_1" AS "segments_3"
+    WHERE
+      ("__8"."x" BETWEEN "segments_3"."x1" AND "segments_3"."x2") AND
+      ("__8"."y" BETWEEN "segments_3"."y1" AND "segments_3"."y2")
+  )))
+),
+"reachable_1" ("x", "y") AS (
+  SELECT
+    "__9"."x",
+    "__9"."y"
+  FROM "__5" AS "__9"
+),
+"__10" ("x", "y") AS (
+  SELECT
+    "reachable_2"."x",
+    "reachable_2"."y"
+  FROM "reachable_1" AS "reachable_2"
+  CROSS JOIN "max_y_1" AS "max_y_3"
+  WHERE ("reachable_2"."y" = "max_y_3"."max_y")
+  UNION ALL
+  SELECT
+    "reachable_4"."x",
+    "reachable_4"."y"
+  FROM (
+    SELECT DISTINCT
+      ("reachable_3"."x" + "values_4"."dx") AS "x",
+      ("reachable_3"."y" - 1) AS "y"
+    FROM "__10" AS "reachable_3"
+    CROSS JOIN (
+      SELECT "values_3"."dx"
+      FROM (
+        VALUES
+          (-1),
+          (0),
+          (1)
+      ) AS "values_3" ("dx")
+    ) AS "values_4"
+  ) AS "reachable_4"
+  WHERE (EXISTS (
+    SELECT NULL
+    FROM "reachable_1" AS "reachable_5"
+    WHERE
+      ("reachable_5"."x" = "reachable_4"."x") AND
+      ("reachable_5"."y" = "reachable_4"."y")
+  ))
+),
+"fallthrough_1" ("x", "y") AS (
+  SELECT
+    "__11"."x",
+    "__11"."y"
+  FROM "__10" AS "__11"
+),
+"__12" ("x", "y") AS (
+  SELECT
+    500 AS "x",
+    0 AS "y"
+  UNION ALL
+  SELECT
+    "__15"."x",
+    "__15"."y"
+  FROM (
+    SELECT DISTINCT
+      "__14"."x",
+      "__14"."y"
+    FROM (
+      SELECT
+        ("__13"."x" + "values_6"."dx") AS "x",
+        ("__13"."y" + 1) AS "y",
+        "values_6"."dx",
+        (NOT (EXISTS (
+          SELECT NULL
+          FROM "fallthrough_1" AS "fallthrough_2"
+          WHERE
+            ("fallthrough_2"."x" = "__13"."x") AND
+            ("fallthrough_2"."y" = ("__13"."y" + 1))
+        ))) AS "down_rest",
+        (NOT (EXISTS (
+          SELECT NULL
+          FROM "fallthrough_1" AS "fallthrough_3"
+          WHERE
+            ("fallthrough_3"."x" = ("__13"."x" - 1)) AND
+            ("fallthrough_3"."y" = ("__13"."y" + 1))
+        ))) AS "down_left_rest"
+      FROM "__12" AS "__13"
+      CROSS JOIN (
+        SELECT "values_5"."dx"
+        FROM (
+          VALUES
+            (-1),
+            (0),
+            (1)
+        ) AS "values_5" ("dx")
+      ) AS "values_6"
+    ) AS "__14"
+    WHERE
+      ("__14"."dx" = 0) OR
+      (("__14"."dx" = -1) AND "__14"."down_rest") OR
+      (("__14"."dx" = 1) AND "__14"."down_rest" AND "__14"."down_left_rest")
+  ) AS "__15"
+  WHERE (EXISTS (
+    SELECT NULL
+    FROM "reachable_1" AS "reachable_6"
+    WHERE
+      ("reachable_6"."x" = "__15"."x") AND
+      ("reachable_6"."y" = "__15"."y")
+  ))
+)
+SELECT
+  "__17"."part1",
+  "reachable_8"."part2"
+FROM (
+  SELECT count(*) AS "part1"
+  FROM "__12" AS "__16"
+  WHERE (NOT (EXISTS (
+    SELECT NULL
+    FROM "fallthrough_1" AS "fallthrough_4"
+    WHERE
+      ("fallthrough_4"."x" = "__16"."x") AND
+      ("fallthrough_4"."y" = "__16"."y")
+  )))
+) AS "__17"
+CROSS JOIN (
+  SELECT count(*) AS "part2"
+  FROM "reachable_1" AS "reachable_7"
+) AS "reachable_8"
