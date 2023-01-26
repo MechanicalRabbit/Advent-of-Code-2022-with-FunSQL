6738441dd071d462e94a92b65ef5ca2dd54ecd61
Kyrylo Simonov <xi@resolvent.net>
2023-01-26 00:28:51 +0000

    Day 15 with PostgreSQL

---
+++ day15-postgresql.jl
@@ -0,0 +1,128 @@
+using FunSQL
+using LibPQ
+using DBInterface
+
+# Make LibPQ compatible with DBInterface.
+DBInterface.connect(::Type{LibPQ.Connection}, args...; kws...) =
+    LibPQ.Connection(args...; kws...)
+
+DBInterface.prepare(conn::LibPQ.Connection, args...; kws...) =
+    LibPQ.prepare(conn, args...; kws...)
+
+DBInterface.execute(conn::Union{LibPQ.Connection, LibPQ.Statement}, args...; kws...) =
+    LibPQ.execute(conn, args...; kws...)
+
+@funsql begin
+
+array_get(a, i) =
+    `?[?]`(a, i)
+
+as_integer(str) =
+    `(?::integer)`(str)
+
+as_bigint(str) =
+    `(?::bigint)`(str)
+
+parse_positions() =
+    begin
+        from(
+            regexp_matches(
+                :input,
+                "Sensor at x=(\\S+), y=(\\S+): closest beacon is at x=(\\S+), y=(\\S+)",
+                "g"),
+            columns = [captures])
+        define(
+            sx => as_integer(array_get(captures, 1)),
+            sy => as_integer(array_get(captures, 2)),
+            bx => as_integer(array_get(captures, 3)),
+            by => as_integer(array_get(captures, 4)))
+        define(range => abs(sx - bx) + abs(sy - by))
+    end
+
+merge_intervals() =
+    begin
+        partition(order_by = [l], frame = (mode = rows, finish = -1))
+        define(bound => l <= max[r] + 1 ? 0 : 1)
+        partition(order_by = [l, -bound], frame = rows)
+        define(interval => sum[bound])
+        group(interval)
+        define(
+            l => min[l],
+            r => max[r])
+    end
+
+solve_part1() =
+    begin
+        from(positions)
+        define(dist => abs(:y - sy))
+        filter(dist <= range)
+        define(
+            l => sx - range + dist,
+            r => sx + range - dist)
+        merge_intervals()
+        group()
+        cross_join(beacons => from(positions).filter(by == :y).group())
+        define(
+            part1 => sum[r - l + 1] - beacons.count_distinct[bx])
+    end
+
+in_range(x, y) =
+    abs(sx - x) + abs(sy - y) <= range
+
+is_covered(X, Y, W, H) =
+    begin
+        from(positions)
+        filter(
+            in_range(:X, :Y) &&
+            in_range(:X + :W - 1, :Y) &&
+            in_range(:X, :Y + :H - 1) &&
+            in_range(:X + :W - 1, :Y + :H - 1))
+        bind(:X => X, :Y => Y, :W => W, :H => H)
+    end
+
+subdivide() =
+    begin
+        filter(w > 1 || h > 1)
+        cross_join(from((; part = [0, 1])))
+        define(
+            x => x + (part == 1 && w > h ? w / 2 : 0),
+            y => y + (part == 1 && w <= h ? h / 2 : 0),
+            w => w <= h ? w : part == 0 ? w / 2 : w - w / 2,
+            h => w > h ? h : part == 0 ? h / 2 : h - h / 2)
+        filter(not_exists(is_covered(x, y, w, h)))
+    end
+
+solve_part2() =
+    begin
+        define(
+            x => 0,
+            y => 0,
+            w => :size + 1,
+            h => :size + 1)
+        iterate(subdivide())
+        filter(w == 1 && h == 1)
+        select(part2 => x * as_bigint(4000000) + y)
+    end
+
+solve_all() =
+    let positions = parse_positions()
+        solve_part1().cross_join(solve_part2())
+    end
+
+const q = solve_all()
+
+end # @funsql
+
+if isempty(ARGS)
+    println(FunSQL.render(q, dialect = :postgresql))
+else
+    const db = DBInterface.connect(FunSQL.DB{LibPQ.Connection}, "")
+    for file in ARGS
+        input = read(file, String)
+        sample = endswith(file, ".sample-input")
+        y = sample ? 10 : 2000000
+        size = sample ? 20 : 4000000
+        output = first(DBInterface.execute(db, q; input, y, size))
+        println("[$file] part1: $(output.part1), part2: $(output.part2)")
+    end
+end
---
+++ day15-postgresql.sql
@@ -0,0 +1,113 @@
+WITH RECURSIVE "positions_1" ("sx", "sy", "range", "bx", "by") AS (
+  SELECT
+    "regexp_matches_2"."sx",
+    "regexp_matches_2"."sy",
+    (abs(("regexp_matches_2"."sx" - "regexp_matches_2"."bx")) + abs(("regexp_matches_2"."sy" - "regexp_matches_2"."by"))) AS "range",
+    "regexp_matches_2"."bx",
+    "regexp_matches_2"."by"
+  FROM (
+    SELECT
+      ("regexp_matches_1"."captures"[1]::integer) AS "sx",
+      ("regexp_matches_1"."captures"[2]::integer) AS "sy",
+      ("regexp_matches_1"."captures"[3]::integer) AS "bx",
+      ("regexp_matches_1"."captures"[4]::integer) AS "by"
+    FROM regexp_matches($1, 'Sensor at x=(\S+), y=(\S+): closest beacon is at x=(\S+), y=(\S+)', 'g') AS "regexp_matches_1" ("captures")
+  ) AS "regexp_matches_2"
+),
+"__1" ("x", "y", "w", "h") AS (
+  SELECT
+    0 AS "x",
+    0 AS "y",
+    ($2 + 1) AS "w",
+    ($2 + 1) AS "h"
+  UNION ALL
+  SELECT
+    "__4"."x",
+    "__4"."y",
+    "__4"."w",
+    "__4"."h"
+  FROM (
+    SELECT
+      ("__3"."x" + (CASE WHEN (("values_2"."part" = 1) AND ("__3"."w" > "__3"."h")) THEN ("__3"."w" / 2) ELSE 0 END)) AS "x",
+      ("__3"."y" + (CASE WHEN (("values_2"."part" = 1) AND ("__3"."w" <= "__3"."h")) THEN ("__3"."h" / 2) ELSE 0 END)) AS "y",
+      (CASE WHEN ("__3"."w" <= "__3"."h") THEN "__3"."w" WHEN ("values_2"."part" = 0) THEN ("__3"."w" / 2) ELSE ("__3"."w" - ("__3"."w" / 2)) END) AS "w",
+      (CASE WHEN ("__3"."w" > "__3"."h") THEN "__3"."h" WHEN ("values_2"."part" = 0) THEN ("__3"."h" / 2) ELSE ("__3"."h" - ("__3"."h" / 2)) END) AS "h"
+    FROM (
+      SELECT
+        "__2"."x",
+        "__2"."w",
+        "__2"."h",
+        "__2"."y"
+      FROM "__1" AS "__2"
+      WHERE
+        ("__2"."w" > 1) OR
+        ("__2"."h" > 1)
+    ) AS "__3"
+    CROSS JOIN (
+      SELECT "values_1"."part"
+      FROM (
+        VALUES
+          (0),
+          (1)
+      ) AS "values_1" ("part")
+    ) AS "values_2"
+  ) AS "__4"
+  WHERE (NOT EXISTS (
+    SELECT NULL
+    FROM "positions_1" AS "positions_10"
+    WHERE
+      ((abs(("positions_10"."sx" - "__4"."x")) + abs(("positions_10"."sy" - "__4"."y"))) <= "positions_10"."range") AND
+      ((abs(("positions_10"."sx" - (("__4"."x" + "__4"."w") - 1))) + abs(("positions_10"."sy" - "__4"."y"))) <= "positions_10"."range") AND
+      ((abs(("positions_10"."sx" - "__4"."x")) + abs(("positions_10"."sy" - (("__4"."y" + "__4"."h") - 1)))) <= "positions_10"."range") AND
+      ((abs(("positions_10"."sx" - (("__4"."x" + "__4"."w") - 1))) + abs(("positions_10"."sy" - (("__4"."y" + "__4"."h") - 1)))) <= "positions_10"."range")
+  ))
+)
+SELECT
+  ("positions_8"."sum" - "beacons_1"."count_distinct") AS "part1",
+  "__6"."part2"
+FROM (
+  SELECT sum((("positions_7"."r" - "positions_7"."l") + 1)) AS "sum"
+  FROM (
+    SELECT
+      max("positions_6"."r") AS "r",
+      min("positions_6"."l") AS "l"
+    FROM (
+      SELECT
+        (sum("positions_5"."bound") OVER (ORDER BY "positions_5"."l", (- "positions_5"."bound") ROWS UNBOUNDED PRECEDING)) AS "interval",
+        "positions_5"."r",
+        "positions_5"."l"
+      FROM (
+        SELECT
+          "positions_4"."r",
+          "positions_4"."l",
+          (CASE WHEN ("positions_4"."l" <= ((max("positions_4"."r") OVER (ORDER BY "positions_4"."l" ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING)) + 1)) THEN 0 ELSE 1 END) AS "bound"
+        FROM (
+          SELECT
+            (("positions_3"."sx" + "positions_3"."range") - "positions_3"."dist") AS "r",
+            (("positions_3"."sx" - "positions_3"."range") + "positions_3"."dist") AS "l"
+          FROM (
+            SELECT
+              "positions_2"."sx",
+              "positions_2"."range",
+              abs(($3 - "positions_2"."sy")) AS "dist"
+            FROM "positions_1" AS "positions_2"
+          ) AS "positions_3"
+          WHERE ("positions_3"."dist" <= "positions_3"."range")
+        ) AS "positions_4"
+      ) AS "positions_5"
+    ) AS "positions_6"
+    GROUP BY "positions_6"."interval"
+  ) AS "positions_7"
+) AS "positions_8"
+CROSS JOIN (
+  SELECT count(DISTINCT "positions_9"."bx") AS "count_distinct"
+  FROM "positions_1" AS "positions_9"
+  WHERE ("positions_9"."by" = $3)
+) AS "beacons_1"
+CROSS JOIN (
+  SELECT (("__5"."x" * (4000000::bigint)) + "__5"."y") AS "part2"
+  FROM "__1" AS "__5"
+  WHERE
+    ("__5"."w" = 1) AND
+    ("__5"."h" = 1)
+) AS "__6"
